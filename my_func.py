1c3b23647737fb9e7b2221a160f493c907e265ca
bsamaha <59106056+bsamaha@users.noreply.github.com>
2020-06-01 21:52:56 -0500

    Monday Update
    Monday Update

---
+++ my_func.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import seaborn as sns
+
+
+def missing_data(data):
+    percentage_missing = round((data.isnull().sum()/len(data))*(100),3)
+    data_types = data.dtypes
+    NaN_count = data.isnull().sum()
+    missing_values = pd.DataFrame({'data_type': data_types,'pct_missing':percentage_missing, 'missing_count': NaN_count})
+    return missing_values
+
+def df_value_counts(df):
+    for column in df:
+        print('**************** Column Name:',column,'****************')
+        print(df[column].value_counts())
+        print('-'*100)
+        print()
+    return None
+
