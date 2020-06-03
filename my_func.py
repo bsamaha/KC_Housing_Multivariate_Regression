6147d6fa64368ad5d59e9413c0b3dba91282b9f8
bsamaha <59106056+bsamaha@users.noreply.github.com>
2020-06-03 10:32:09 -0500

    Wednesday Update

--- my_func.py
+++ my_func.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
+
+import statsmodels.api as sm
+from sklearn.base import BaseEstimator, RegressorMixin
 
 
 def missing_data(data):
@@ -18,6 +22,20 @@ def df_value_counts(df):
     return None
 
 def log_to_dollars(number):
-    dollars = 10**number
+    dollars = np.e**number
     return dollars.round(2)
 
+class SMWrapper(BaseEstimator, RegressorMixin):
+    """ A universal sklearn-style wrapper for statsmodels regressors """
+    def __init__(self, model_class, fit_intercept=True):
+        self.model_class = model_class
+        self.fit_intercept = fit_intercept
+    def fit(self, X, y):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        self.model_ = self.model_class(y, X)
+        self.results_ = self.model_.fit()
+    def predict(self, X):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        return self.results_.predict(X)
